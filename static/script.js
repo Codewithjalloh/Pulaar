146a9f8f29085a898eb0089d2addaa501176501c
D.A.J <codewithjalloh@gmail.com>
2023-11-30 19:54:30 +0000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -18,3 +18,31 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch((error) => console.error('Error:', error));
     });
 });
+
+// Clear button functionality
+document.getElementById('clearButton').addEventListener('click', function () {
+  document.getElementById('englishText').value = '';
+  document.getElementById('translationResult').textContent = '';
+});
+
+// Speak button functionality (Text-to-Speech)
+document.getElementById('speakButton').addEventListener('click', function () {
+  const text = document.getElementById('translationResult').textContent;
+  if (text) {
+    const speech = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(speech);
+  }
+});
+
+// Voice Record button functionality (Speech-to-Text) - Simplified example
+document
+  .getElementById('voiceRecordButton')
+  .addEventListener('click', function () {
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = 'en-US';
+    recognition.onresult = function (event) {
+      document.getElementById('englishText').value =
+        event.results[0][0].transcript;
+    };
+    recognition.start();
+  });
