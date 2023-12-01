41d44efa3f82048b2bf6a46d6b4a2c5a6066ca3f
D.A.J <codewithjalloh@gmail.com>
2023-12-01 19:31:46 +0000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Translate button functionality
   document
     .getElementById('translateButton')
     .addEventListener('click', function () {
@@ -12,37 +13,52 @@ document.addEventListener('DOMContentLoaded', function () {
       })
         .then((response) => response.json())
         .then((data) => {
-          document.getElementById('translationResult').textContent =
-            data.translation;
+          const translationResult =
+            document.getElementById('translationResult');
+          translationResult.textContent = data.translation;
+
+          // Add the English word and its translation to the history list if valid
+          if (
+            data.translation &&
+            data.translation !== 'Translation not found'
+          ) {
+            const translationsList =
+              document.getElementById('translationsList');
+            const newListItem = document.createElement('li');
+            newListItem.textContent = englishWord + ' - ' + data.translation;
+            translationsList.insertBefore(
+              newListItem,
+              translationsList.firstChild
+            );
+          }
         })
         .catch((error) => console.error('Error:', error));
     });
-});
 
-// Clear button functionality
-document.getElementById('clearButton').addEventListener('click', function () {
-  document.getElementById('englishText').value = '';
-  document.getElementById('translationResult').textContent = '';
-});
-
-// Speak button functionality (Text-to-Speech)
-document.getElementById('speakButton').addEventListener('click', function () {
-  const text = document.getElementById('translationResult').textContent;
-  if (text) {
-    const speech = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(speech);
-  }
-});
-
-// Voice Record button functionality (Speech-to-Text) - Simplified example
-document
-  .getElementById('voiceRecordButton')
-  .addEventListener('click', function () {
-    const recognition = new webkitSpeechRecognition();
-    recognition.lang = 'en-US';
-    recognition.onresult = function (event) {
-      document.getElementById('englishText').value =
-        event.results[0][0].transcript;
-    };
-    recognition.start();
+  // Clear button functionality
+  document.getElementById('clearButton').addEventListener('click', function () {
+    document.getElementById('englishText').value = '';
+    document.getElementById('translationResult').textContent = '';
   });
+
+  document.getElementById('speakButton').addEventListener('click', function () {
+    const text = document.getElementById('translationResult').textContent;
+    if (text) {
+      const speech = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech);
+    }
+  });
+
+  // Voice Record button functionality (Speech-to-Text)
+  document
+    .getElementById('voiceRecordButton')
+    .addEventListener('click', function () {
+      const recognition = new webkitSpeechRecognition();
+      recognition.lang = 'en-US';
+      recognition.onresult = function (event) {
+        document.getElementById('englishText').value =
+          event.results[0][0].transcript;
+      };
+      recognition.start();
+    });
+});
